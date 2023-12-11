7dc70a3990c354fbe1e91dbd19ae87f3d4fd188c
emmanuel julio <emajulio.ej@gmail.com>
2023-12-11 16:21:06 -0300

    Primer push

---
+++ Clase1/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+  Clase 1: Introducción a C# y Configuración del Entorno
+        **Contenido:**
+- Presentación del lenguaje C#.
+- Instalación y configuración de Visual Studio.
+- Creación de un nuevo proyecto en Visual Studio.
+
+ */
