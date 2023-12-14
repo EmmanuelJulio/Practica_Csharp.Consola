40f975b71a294c5f4c0d6d4a64647b164cd1a88c
Gustavo <gustavomatiasmoraless@gmail.com>
2023-12-14 02:51:43 +0100

    Codigo de la clase de variables

---
+++ Clase1/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    public class Auto
+    {
+        private string Color { get; set; }
+        private int NumeroDePuertas { get; set; }
+        private string Marca { get; set; }
+
+        public Auto(string Color, int NumeroDePuertas , string Marca)
+        {
+            this.Marca = Marca;
+            this.NumeroDePuertas = NumeroDePuertas;
+            this.Color = Color;
+        }
+        public string GetColor()
+        {
+            return this.Color;
+        }
+    }
+}
--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -6,3 +6,38 @@
 - Creación de un nuevo proyecto en Visual Studio.
 
  */
+
+/*  Definir dos variables numericas a y b e imprimir por consola: 
+    a   Su suma.
+    b   Su multiplicacion.
+    c   Su Resta.
+*/
+
+//string Nombre = "Gustavo";
+//int numero = 1;
+
+//String A = "Suma, multiplicaciónm, resta";
+//int suma = 2;
+//int multiplicación = 2;
+//int resta = 4;
+
+//Console.WriteLine(suma);
+
+//String B = "Suma, multiplicación. resta";
+
+//int a = 2;
+//int b = 3;
+//Console.WriteLine(a + b);
+//Console.WriteLine(a*b);
+//Console.WriteLine(a-b);
+
+/*
+ Defina dos variables del tipo string con dos nombres distintos en dos variables nombre1 y nombre2, imprima por pantalla "nombre1" y nombre2 ,son amigos hace mucho tiempo;
+recuerde usar string interpolation a la hora de colocar el mensaje dentro del console.writeline($"{nombre1} y {nombre2} ,son amigos hace mucho tiempo")
+ 
+ */
+
+string nombre1 = "Gustavo";
+string nombre2 = "Julio";
+Console.WriteLine($"{nombre1} y {nombre2} ,son amigos hace mucho tiempo");
+
